c99e98ff69f1b3c8ae6e744d4464bb9395a7c205
Antonio Paonessa <132266601+apaonessaa@users.noreply.github.com>
2025-02-19 12:24:26 +0100

    init fragments

---
+++ app/src/main/java/com/example/wellme/HomeFragment.kt
@@ -0,0 +1,39 @@
+package com.example.wellme
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Infliamo il layout del Fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Gestiamo i pulsanti
+        val btnStart = view.findViewById<Button>(R.id.startActivity_button)
+        btnStart.setOnClickListener {
+            val intent = Intent(activity, FocusActivity::class.java)
+            intent.putExtra("activityType", "walking")
+            startActivity(intent)
+        }
+
+        // Sezione: Stato d'animo
+        val btnAnnota = view.findViewById<Button>(R.id.btnAnnota)
+        btnAnnota.setOnClickListener {
+            val intent = Intent(activity, MoodActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/wellme/MainActivity.kt
+++ app/src/main/java/com/example/wellme/MainActivity.kt
@@ -1,28 +1,41 @@
 package com.example.wellme
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    val activities =
+        arrayOf("WALKING", "RUNNING", "WEIGHTLIFTING", "YOGA", "BASKETBALL", "SOCCER",
+            "TENNIS", "TABLE TENNIS", "BOXING", "GOLF")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnStart = findViewById<Button>(R.id.startActivity_button)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
-        btnStart.setOnClickListener {
-            val intent = Intent(this, FocusActivity::class.java)
-            intent.putExtra("activityType", "walking")
-            startActivity(intent)
+        // Caricare il primo fragment all'avvio
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
         }
 
-        // Sezione: Stato d'animo
-        val btnAnnota = findViewById<Button>(R.id.btnAnnota)
-        btnAnnota.setOnClickListener {
-            val intent = Intent(this, MoodActivity::class.java)
-            startActivity(intent)
+        // Gestire il click sui pulsanti della Bottom Navigation
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val selectedFragment = when (item.itemId) {
+                R.id.nav_home -> HomeFragment()
+                //R.id.nav_activity -> ActivityFragment()
+                //R.id.nav_profile -> ProfileFragment()
+                else -> null
+            }
+            selectedFragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, it)
+                    .commit()
+            }
+            true
         }
     }
 }
